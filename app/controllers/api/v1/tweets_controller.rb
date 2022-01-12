693b09c901b8c17b1c907ebc3c58a928dc46069f
SaraswatiSaud <saraswati.saud@gmail.com>
2022-01-12 17:25:29 -0500

    Implement tweet CRUD features

--- app/controllers/api/v1/tweets_controller.rb
+++ app/controllers/api/v1/tweets_controller.rb
@@ -1,12 +1,55 @@
+# frozen_string_literal: true
+
 module Api
   module V1
     class TweetsController < ApplicationController
+      before_action :authenticate_api_v1_user!
+      before_action :find_params, only: %i[show update destroy]
+
       def index
-        tweets = Tweet.all
+        tweets = current_api_v1_user.tweets.all
         render json: tweets, status: 200
       end
 
+      def create
+        tweet = current_api_v1_user.tweets.new(
+          content: tweet_params[:content]
+        )
+        if tweet.save
+          render json: tweet, status: 200
+        else
+          render json: { error: 'Error while creating tweet' }
+        end
+      end
+
       def show
+        render json: @tweet, status: 200
+      end
+
+      def update
+        if @tweet.update(tweet_params)
+          render json: @tweet, status: 200
+        else
+          render json: { error: 'Error while updating tweet' }
+        end
+      end
+
+      def destroy
+        if @tweet.destroy
+          render json: { success: 'Tweet deleted successfully' }
+        else
+          render json: { error: 'Error while deleting tweet' }
+        end
+      end
+
+      private
+
+      def find_params
+        @tweet = current_api_v1_user.tweets.find(params[:id])
+      end
+
+      def tweet_params
+        params.require(:tweet).permit(:id, :content, :user_id)
       end
     end
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,11 @@
 class User < ActiveRecord::Base
 
   extend Devise::Models #added this line to extend devise model
-  
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   include DeviseTokenAuth::Concerns::User
+  has_many :tweets
 end
